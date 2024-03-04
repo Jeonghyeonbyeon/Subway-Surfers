f59c8264132abc5e604a40950ba66eee7dd36ec6
JunghyunByun <bjh1206408@gmail.com>
2024-03-04 23:37:36 +0900

    commit

--- Subway Surfers/Assets/3.Scripts/Player/PlayerController.cs
+++ Subway Surfers/Assets/3.Scripts/Player/PlayerController.cs
@@ -19,28 +19,27 @@ public class PlayerController : MonoBehaviour
     private void Update()
     { 
         float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(x * playerSpeed * Time.deltaTime, 0f, 0f);
+        transform.Translate(x * playerSpeed * Time.deltaTime, (y == 0 ? 0 : -1) * playerSpeed * Time.deltaTime, 0f);
 
-        animator.Play("Run");
-
+        if (Physics.Raycast(transform.position + Vector3.up * 0.25f, Vector3.down, out RaycastHit hit, 0.25f, LayerMask.GetMask("Ground"))) animator.Play("Run");
+        
+        else animator.Play("Jump");
+        
         OnDrawRay();
     }
 
     private void OnJump()
     {
-        if (Physics.Raycast(transform.position + Vector3.up * 0.25f, Vector3.down, out RaycastHit hit, 0.5f, LayerMask.GetMask("Ground")))
+        if (Physics.Raycast(transform.position + Vector3.up * 0.25f, Vector3.down, out RaycastHit hit, 0.25f, LayerMask.GetMask("Ground")))
         {
-
-            Debug.Log("Ground");
-
-            playerRigid.velocity = Vector3.up * 100f;
-
+            playerRigid.velocity = Vector3.up * 8.5f;
         }
     }
 
     private void OnDrawRay()
     {
-        Debug.DrawRay(transform.position, Vector3.down * 0.5f, new Color(0, 1, 0));
+        Debug.DrawRay(transform.position, Vector3.down * 0.25f, new Color(0, 1, 0));
     }
 }
